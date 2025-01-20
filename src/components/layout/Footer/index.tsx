92f60ec872332532f5e0d8d08152c87a8c65672f
sujung lee <sujunglee@dejay.co.kr>
2025-01-20 11:16:46 +0900

    Style: Footer 메세지 작성

--- src/components/layout/Footer/index.tsx
+++ src/components/layout/Footer/index.tsx
@@ -1,5 +1,19 @@
 import styles from './Footer.module.css'
 
 export default function Footer() {
-  return <footer className={styles.wrapper}>Footer</footer>
+  return (
+    <footer className={styles.wrapper}>
+      <h3>Netclickx Korea</h3>
+      <p>
+        This is a Netflix clone website created for educational purposes.
+        <br />
+        It aims to showcase web development skills by replicating key features
+        of the Netflix platform.
+        <br />
+        Please note that it is not affiliated with or endorsed by Netflix, Inc.
+        <br />
+        2025-01-20
+      </p>
+    </footer>
+  )
 }
